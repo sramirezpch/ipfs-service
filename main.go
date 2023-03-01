a642c878bfd3fdab28f0d3b65af7910a0e408fc6
Sergio Ramirez <sergio.ramirez@thebeat.co>
2023-02-28 19:43:21 -0500

    feat: add json encoding to router

--- main.go
+++ main.go
@@ -8,12 +8,21 @@ import (
 	w "github.com/sramirezpch/ipfs-writer/src/writer"
 )
 
+func contentTypeApplicationJsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		next.ServeHTTP(w, r)
+	})
+}
 func NewRouter() *mux.Router {
 	ipfsWriter := w.NewIPFSWriter()
+
 	ipfsWriterHandler := &w.IPFSWriterHandler{Writer: ipfsWriter}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/pin-file", ipfsWriterHandler.HandlePinFile).Methods("POST")
+	r.Use(contentTypeApplicationJsonMiddleware)
 
 	return r
 }
