333a6ec14d3c6088bd0fa7ea59295cd8cf32d65a
Sergio Ramirez <sergio.ramirez@thebeat.co>
2023-02-28 21:58:12 -0500

    fix: add CORS headers

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/sramirezpch/ipfs-writer
 go 1.20
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.3
+	github.com/wabarc/ipfs-pinner v1.1.0
+)
+
+require (
 	github.com/wabarc/helper v0.0.0-20211225065210-3d35291efe54 // indirect
-	github.com/wabarc/ipfs-pinner v1.1.0 // indirect
 	github.com/ybbus/httpretry v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
 	golang.org/x/text v0.3.6 // indirect
--- main.go
+++ main.go
@@ -5,16 +5,10 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	w "github.com/sramirezpch/ipfs-writer/src/writer"
 )
 
-func contentTypeApplicationJsonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
-		next.ServeHTTP(w, r)
-	})
-}
 func NewRouter() *mux.Router {
 	ipfsWriter := w.NewIPFSWriter()
 
@@ -22,13 +16,16 @@ func NewRouter() *mux.Router {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/pin-file", ipfsWriterHandler.HandlePinFile).Methods("POST")
-	r.Use(contentTypeApplicationJsonMiddleware)
 
 	return r
 }
 
 func main() {
 	router := NewRouter()
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"*"},
+	})
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", c.Handler(router)))
 }
